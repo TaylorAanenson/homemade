3914a40f5f756dd52d3d56bcfe88baab420cf51d
chibimayura <chibimayura@gmail.com>
2018-10-23 20:13:46 -0700

    create AddPost.js

--- client/app/components/Post/AddPost.js
+++ client/app/components/Post/AddPost.js
@@ -1,40 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Alert, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Alert, ActivityIndicator, Button } from 'react-native';
 
 export default class AddPost extends React.Component {
 	render() {
 		return(
 			<View style={styles.container}>
-				<View style={{flex: 1, flexDirection: 'column'}}>
-				<Text>Add a Title: </Text>
-				<TextInput placeholder="Add a Title" />
-				</View>
+				 <View style={styles.postStyle}>
+			        <Text>Title: </Text>
+					<TextInput style={styles.inputStyle} placeholder="Title..." />
+        
+			        <Text style={styles.spacing} >Description:</Text>
+					<TextInput style={styles.inputStyle} multiline={true} placeholder="Description..." />
+
+			        <Button color="tomato" title="Submit"/>
+		        </View>
 			</VIew>	
 		)
 	}
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginTop: 100,
     alignSelf: 'stretch',
-    flexDirection: 'row'
-  },
-  textStyle: {
-    fontSize: 20,
-  },
-  priceStyle:{
-    fontSize: 20,
-    color: 'green',
-    alignItems:'right',
-    justifyContent: 'flex-end',
   },
   postStyle: {
-    backgroundColor: 'white',
+    backgroundColor: 'orange',
+  },
+  inputStyle: {
     borderWidth: 1,
-    borderColor: 'grey',
+    borderColor: 'grey'
+  },
+  spacing: {
+    marginTop: 10
   }
 });
+
